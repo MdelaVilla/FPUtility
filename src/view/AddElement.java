7c5981c0f8cdbe49c1810508cbcdfce87c016c35
samuellopz <samuellopz@samuellopz>
2017-01-16 17:39:36 +0100

    Modelo sin tantas clases xD

--- src/view/AddElement.java
+++ src/view/AddElement.java
@@ -34,6 +34,7 @@ public class AddElement extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         jLabel1.setFont(new java.awt.Font("Ubuntu", 1, 18)); // NOI18N
         jLabel1.setText("Añadir elemento");
